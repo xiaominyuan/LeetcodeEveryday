1eb89f7e2940a5f30db38cb2e6000d844e49b78e
yuanxiaomin <yuanxiaomin1995@126.com>
2019-07-06 10:57:07 +0800

    90

---
+++ src/main/java/backTracking/code90.java
@@ -0,0 +1,88 @@
+//90. 子集 II
+//        给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+//
+//        说明：解集不能包含重复的子集。
+//
+//        示例:
+//
+//        输入: [1,2,2]
+//        输出:
+//        [
+//        [2],
+//        [1],
+//        [1,2,2],
+//        [2,2],
+//        [1,2],
+//        []
+//        ]
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/subsets-ii
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+package backTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//先把数组排序，每次回溯的时候，碰见前后相同的数字就跳出当前循环，继续下一个循环
+
+public class code90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        quickSort(nums,0,nums.length-1);
+
+        getResult(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+
+    public void getResult(int[] nums, int start, ArrayList<Integer> tmp, List<List<Integer>> result){
+        result.add(new ArrayList(tmp));
+
+        for (int i = start; i<nums.length; i++){
+            if (i>start && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            tmp.add(nums[i]);
+            getResult(nums, i+1, tmp, result);
+            tmp.remove(tmp.size()-1);
+        }
+    }
+
+    public void quickSort(int[] nums, int low, int high){
+
+        if (low>high){
+            return;
+        }
+
+        int left = low+1;
+        int right = high;
+        int tmp = nums[low];
+
+        while (left <= right){
+            while(left<right && nums[left]<=tmp){
+                left++;
+            }
+            while (nums[right] > tmp){
+                right--;
+            }
+
+            if (left < right){
+                int change = nums[left];
+                nums[left] = nums[right];
+                nums[right] = change;
+            }else{
+                break;
+            }
+        }
+
+        nums[low] = nums[right];
+        nums[right] = tmp;
+
+        quickSort(nums,low,right-1);
+        quickSort(nums,right+1,high);
+    }
+}
