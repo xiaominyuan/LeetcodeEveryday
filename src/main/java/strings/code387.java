f4660871bcf96b0d7552d05a067896326cab4fb9
yuanxiaomin <yuanxiaomin1995@126.com>
2019-07-17 23:07:19 +0800

    387

---
+++ src/main/java/strings/code387.java
@@ -0,0 +1,41 @@
+//387. 字符串中的第一个唯一字符
+//        给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+//
+//        案例:
+//
+//        s = "leetcode"
+//        返回 0.
+//
+//        s = "loveleetcode",
+//        返回 2.
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+package strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class code387 {
+    public int firstUniqChar(String s) {
+        int len = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i<len; i++){
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+
+        for (int i = 0; i<len; i++){
+            int tmp = map.get(s.charAt(i));
+            if (tmp == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
