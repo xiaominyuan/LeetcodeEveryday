593d5b12ebf56ab20ba1d8d4bffdd4c1133a03fe
yuanxiaomin <yuanxiaomin1995@126.com>
2019-08-21 00:15:41 +0800

    228

---
+++ src/main/java/Arrays/code228.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class code228 {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0, j= 0; j<nums.length; j++){
+            if (j+1 < nums.length && nums[j]+1 == nums[j+1]){
+                continue;
+            }
+
+            if (i != j){
+                result.add(nums[i] + "->" + nums[j]);
+            }else {
+                result.add(nums[i] + "");
+            }
+
+            i=j+1;
+        }
+
+        return result;
+    }
+}
