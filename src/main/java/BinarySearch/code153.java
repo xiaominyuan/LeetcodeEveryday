3dad8229b59dce3c4d521f6e4502e8cdcb6beafd
yuanxiaomin <yuanxiaomin1995@126.com>
2019-11-09 18:18:38 +0800

    153

---
+++ src/main/java/BinarySearch/code153.java
@@ -0,0 +1,81 @@
+//153. 寻找旋转排序数组中的最小值
+//假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+//
+//( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+//
+//请找出其中最小的元素。
+//
+//你可以假设数组中不存在重复元素。
+//
+//示例 1:
+//
+//输入: [3,4,5,1,2]
+//输出: 1
+//示例 2:
+//
+//输入: [4,5,6,7,0,1,2]
+//输出: 0
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+package BinarySearch;
+
+public class code153 {
+    public int findMin(int[] nums) {
+        int len = nums.length;
+        int count = 0;
+
+        for (int i = 0; i< len -1; i++){
+            if (nums[i] <= nums[i+1]){
+                count++;
+            }else{
+                break;
+            }
+        }
+
+        if (len == (count + 1)){
+            return nums[0];
+        }else{
+            return nums[count+1];
+        }
+    }
+
+    public int findMin2(int[] nums) {
+        if (nums.length == 1){
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.length-1;
+
+        if (nums[right] >= nums[0]){
+            //没旋转
+            return nums[0];
+        }
+
+        while (left <= right){
+            int mid = (left + right)/2;
+
+            if (nums[mid+1] < nums[mid]){
+                return nums[mid+1];
+            }
+
+            if (nums[mid-1] < nums[mid]){
+                return nums[mid];
+            }
+
+            if (nums[mid] < nums[right]){
+                right = mid -1;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+
+
+    }
+
+}
