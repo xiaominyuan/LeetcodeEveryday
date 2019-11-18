9f4c4cb78c773b563ccab24a83c028bf82cb4515
yuanxiaomin <yuanxiaomin1995@126.com>
2019-11-18 22:51:19 +0800

    14

---
+++ src/main/java/jianzhioffer/code14.java
@@ -0,0 +1,43 @@
+//二叉树的镜像
+//操作给定的二叉树，将其变换为源二叉树的镜像。
+//输入描述:
+//二叉树的镜像定义：源二叉树
+//    	    8
+//    	   /  \
+//    	  6   10
+//    	 / \  / \
+//    	5  7 9 11
+//    	镜像二叉树
+//    	    8
+//    	   /  \
+//    	  10   6
+//    	 / \  / \
+//    	11 9 7  5
+
+package jianzhioffer;
+
+import DFS.TreeNode;
+
+public class code14 {
+    public void Mirror(TreeNode root) {
+        if (root == null){
+            return;
+        }
+
+        if (root.left == null && root.right == null){
+            return;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        if (root.left != null){
+            Mirror(root.left);
+        }
+
+        if (root.right != null){
+            Mirror(root.right);
+        }
+    }
+}
