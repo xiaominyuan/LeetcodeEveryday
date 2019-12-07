286ea44ad30894fd080d7b95685417c241433851
yuanxiaomin <yuanxiaomin1995@126.com>
2019-12-07 22:37:16 +0800

    lru

--- src/main/java/Arrays/code73.java
+++ src/main/java/Arrays/code73.java
@@ -72,6 +72,14 @@ public class code73 {
             }
         }
 
+        for (int i = 1; i< row; i++){
+            for (int j = 1; j<col; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
         if (colFlag){
             for (int i = 0; i<row; i++){
                 matrix[i][0] = 0;
