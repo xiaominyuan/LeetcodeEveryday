3144fae3fa8270bd371dac6314610b18c43dc684
yuanxiaomin <yuanxiaomin1995@126.com>
2020-03-22 20:33:50 +0800

    365

---
+++ src/main/java/DFS/isSubPath1367.java
@@ -0,0 +1,58 @@
+//1367. 二叉树中的列表
+//给你一棵以 root 为根的二叉树和一个 head 为第一个节点的链表。
+//
+//如果在二叉树中，存在一条一直向下的路径，且每个点的数值恰好一一对应以 head 为首的链表中每个节点的值，那么请你返回 True ，否则返回 False 。
+//
+//一直向下的路径的意思是：从树中某个节点开始，一直连续向下的路径。
+//
+// 
+//
+//示例 1：
+//
+//
+//
+//输入：head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+//输出：true
+//解释：树中蓝色的节点构成了与链表对应的子路径。
+//示例 2：
+//
+//
+//
+//输入：head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+//输出：true
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/linked-list-in-binary-tree
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+package DFS;
+
+public class isSubPath1367 {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null){
+            return true;
+        }
+
+        if (root == null){
+            return false;
+        }
+
+        return isSub(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    public boolean isSub(ListNode head, TreeNode node){
+        if (head == null){
+            return true;
+        }
+
+        if (node == null){
+            return false;
+        }
+
+        if (head.val != node.val){
+            return false;
+        }
+
+        return isSub(head.next, node.left) || isSub(head.next, node.right);
+    }
+}
